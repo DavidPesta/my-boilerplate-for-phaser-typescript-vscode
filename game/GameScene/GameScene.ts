6fc76f79d691d0f703c2052c85a529da950a78e6
David Pesta <davidpesta@gmail.com>
2017-12-20 09:25:09 -0600

    Moved loadAssets to top of classes as a convention and turned the boilerplate image into a background.

--- game/GameScene/GameScene.ts
+++ game/GameScene/GameScene.ts
@@ -1,11 +1,14 @@
 class GameScene extends Phaser.State {
 	static loadAssets(game:Phaser.Game) {
 		let assetPath = "game/GameScene";
-		game.load.image("imageImage", assetPath + "/image.png");
+		game.load.image("backgroundImage", assetPath + "/image.png");
 	}
 	
+	backgroundImage:Phaser.Sprite;
+	
 	create() {
-		let image = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "imageImage");
-		image.anchor.setTo(0.5, 0.5);
+		this.backgroundImage = this.game.add.sprite(0, 0, "backgroundImage");
+		this.backgroundImage.width = 1280;
+		this.backgroundImage.height = 720;
 	}
 }
--- game/Preloader/Preloader.ts
+++ game/Preloader/Preloader.ts
@@ -1,10 +1,10 @@
 class Preloader extends Phaser.State {
-	preloadBar:Phaser.Sprite;
-	
 	static loadAssets(game:Phaser.Game) {
 		let assetPath = "game/Preloader";
 		game.load.image("preloadBarImage", assetPath + "/bar.png");
 	}
+	
+	preloadBar:Phaser.Sprite;
 	
 	preload() {
 		this.preloadBar = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, "preloadBarImage");
