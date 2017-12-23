5d8d831c597d6681b4fd5df5654557d7bb731f7c
David Pesta <davidpesta@gmail.com>
2017-12-23 16:57:40 -0600

    Activate the enter key as an additional way to transition to the GameScene.

--- game/Home/Home.ts
+++ game/Home/Home.ts
@@ -8,12 +8,20 @@ class Home extends Phaser.State {
 		this.background.width = this.game.width;
 		this.background.height = this.game.height;
 		this.background.inputEnabled = true;
-		this.background.events.onInputDown.add(() => {
-			Game.fadeOut(this.game, () => {
-				this.game.state.start("GameScene");
-			}, this);
-		});
+		
+		this.background.events.onInputDown.add(this.startGameScene, this);
+		this.game.input.keyboard.onDownCallback = () => {
+			if (13 == this.game.input.keyboard.event.which) {
+				this.startGameScene();
+			}
+		};
 		
 		let gameStartText = new HUDText(this.game, "PRESS TO START", TextSize.Medium, TextColor.White);
+	}
+	
+	startGameScene() {
+		Game.fadeOut(this.game, () => {
+			this.game.state.start("GameScene");
+		}, this);
 	}
 }
